f46a0645c442766422dddb235c5642ed49d5ca1e
Metin Dagcilar <md3g11@soton.ac.uk>
2018-04-09 00:26:41 +0100

    fixed project structure and added empty test file

---
+++ src/test/java/com/m3c/md/controller/BookManagerImplTest.java
@@ -0,0 +1,12 @@
+package com.m3c.md.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookManagerImplTest {
+
+    @Test
+    public void findTopThreeWords() {
+    }
+}
