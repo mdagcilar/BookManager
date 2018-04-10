c3a26dc487732290e34cba906cc75288c3982179
mdagcilar <md3g11@soton.ac.uk>
2018-04-10 09:38:20 +0100

    - created constants file to store all constants

---
+++ src/main/java/com/m3c/md/model/Constants.java
@@ -0,0 +1,17 @@
+package com.m3c.md.model;
+
+/**
+ * Class of constants
+ *
+ * @author Metin Dagcilar
+ * @version 1.0
+ * @since 2018-04-03
+ */
+
+public class Constants {
+    public static final String TURING_FILE = "src/main/resources/turing.txt";
+    public static final String A_LARGE_FILE = "src/main/resources/aLargeFile";
+    public static final String OUTPUT_RESULTS_FILE = "src/main/resources/output.txt";
+    public static final String LOG_PROPERTIES_FILE = "src/main/resources/log4j.properties";
+    public static final String FILE_NOT_FOUND = "File not found ";
+}
